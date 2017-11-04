da99b64e071cf1c2b7d5d3b783031a87562a26fc
tawsif93 <tawsif93@gmail.com>
2017-11-04 17:50:04 +0600

    Assignment 3

---
+++ assignment3/mapper.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# Format of each line is:
+# date\ttime\tstore name\titem description\tcost\tmethod of payment
+#
+# We want elements 2 (store name) and 4 (cost)
+# We need to write them out to standard output, separated by a tab
+
+import sys
+from urlparse import urlparse
+
+for line in sys.stdin:
+	data = line.strip().split('"')
+	if len(data) == 3:
+		mapped= data[1].strip().split(" ")
+		path = urlparse(mapped[1]).path
+		if path:
+			print str(path)
+
---
+++ assignment3/reducer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import sys
+
+salesTotal = 0
+oldKey = None
+
+maxFile = None
+maxCount = 0
+# Loop around the data
+# It will be in the format key\tval
+# Where key is the store name, val is the sale amount
+#
+# All the sales for a particular store will be presented,
+# then the key will change and we'll be dealing with the next store
+
+for line in sys.stdin:
+	thisKey = line
+
+	if oldKey and oldKey != thisKey:
+#	 print oldKey, "\t", salesTotal
+		if maxCount < salesTotal:
+			maxCount = salesTotal
+			maxFile = oldKey
+		oldKey = thisKey;
+		salesTotal = 0
+
+	oldKey = thisKey
+	salesTotal += 1
+if maxCount < salesTotal:
+	maxCount = salesTotal
+	maxFile = oldKey
+
+print maxFile
