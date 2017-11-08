ee2bdd0c2ce63292dedc6793e5296573a6d6f5dd
tawsif93 <tawsif93@gmail.com>
2017-11-08 12:58:50 +0600

    lab 2 updated

--- assignment6/reducer.py
+++ assignment6/reducer.py
@@ -15,16 +15,18 @@ oldKey = None
 C2009 = 0
 C2010 = 0
 C2011 = 0
-
-# line = "2009"
+data = {}
+# line = "GET"
 for line in sys.stdin:
 	mapped = line.strip()
-	if mapped == "2009":
-		C2009+=1
-	if mapped == "2010":
-		C2010+=1
-	if mapped == "2011":
-		C2011+=1
+	if not mapped in data:
+		data[mapped] = 1
+	else: 
+		data[mapped] +=1
+
+
+for k,v in data.items():
+	print ("{0} {1}".format(k,v))
 	# print thisKey, "\t", thisSale
   #  if oldKey and oldKey != thisKey:
    #	 print oldKey, "\t", salesTotal
@@ -35,8 +37,5 @@ for line in sys.stdin:
   #  salesTotal += float(thisSale)
 
 #if oldKey != None:
-print "2009 " + str(C2009) + "\n" + "2010 " + str(C2010) + "\n" + "2011 " + str(C2011) + "\n"
-print C2009
-print C2010
-print C2011
+# print "2009 " + str(C2009) + "\n" + "2010 " + str(C2010) + "\n" + "2011 " + str(C2011) + "\n"
 
