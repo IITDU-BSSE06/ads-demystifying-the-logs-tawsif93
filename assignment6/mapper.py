16c3519f71e21cf1a063d43a2f86ca45b97bfa2d
tawsif93 <tawsif93@gmail.com>
2017-11-08 11:51:38 +0600

    lab1

---
+++ assignment6/mapper.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# Format of each line is:
+# date\ttime\tstore name\titem description\tcost\tmethod of payment
+#
+# We want elements 2 (store name) and 4 (cost)
+# We need to write them out to standard output, separated by a tab
+
+import sys
+from datetime import date
+
+# line = "10.223.157.186 - - [15/Jul/2009:14:58:59 -0700] \"GET / HTTP/1.1\" 403 202"
+for line in sys.stdin:
+	data = line.strip().split('[')
+	data = data[1].strip().split(']')
+	data = data[0].strip().split('/')
+	data = data[2].strip().split(':')
+# if len(data) == 3:
+# 	print data[1]
+	print data[0]
+
---
+++ assignment6/reducer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import sys
+
+salesTotal = 0
+oldKey = None
+
+# Loop around the data
+# It will be in the format key\tval
+# Where key is the store name, val is the sale amount
+#
+# All the sales for a particular store will be presented,
+# then the key will change and we'll be dealing with the next store
+
+C2009 = 0
+C2010 = 0
+C2011 = 0
+
+# line = "2009"
+for line in sys.stdin:
+	mapped = line.strip()
+	if mapped == "2009":
+		C2009+=1
+	if mapped == "2010":
+		C2010+=1
+	if mapped == "2011":
+		C2011+=1
+	# print thisKey, "\t", thisSale
+  #  if oldKey and oldKey != thisKey:
+   #	 print oldKey, "\t", salesTotal
+	#	oldKey = thisKey;
+	 #   salesTotal = 0
+#
+ #   oldKey = thisKey
+  #  salesTotal += float(thisSale)
+
+#if oldKey != None:
+print "2009 " + str(C2009) + "\n" + "2010 " + str(C2010) + "\n" + "2011 " + str(C2011) + "\n"
